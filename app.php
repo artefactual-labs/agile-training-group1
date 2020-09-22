ca2a318094f0d6a318ad089ddf49a3e102407d61
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2020-09-22 12:26:44 -0600

    WIP

--- app.php
+++ app.php
@@ -2,12 +2,25 @@
 
 
 
-function test1()
+function canCreateDescription()
 {
-
+  $myDescription = new Description(500);
+  if ($myDescription->viewDescription() !== "Id: 500")
+  {
+    // raise here
+  }
 }
 
 class Description()
 {
-
+  private
+    $id = 0;
+  public function __construct($id)
+  {
+    $this->id = $id;
+  }
+  public function viewDescription()
+  {
+    printf("Id: %s\n", $this->id);
+  }
 }
